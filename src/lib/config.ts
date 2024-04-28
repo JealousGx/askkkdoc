1235bb64109bea274d1a3d5c2449ee10d0afcc53
Matt <59867217+JealousGx@users.noreply.github.com>
2024-04-28 19:15:37 +0500

    update env

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -29,7 +29,7 @@ export const env = createEnv({
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
 
     AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
-    AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
+    AWS_SECRET_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
 
     OPEN_AI_KEY: process.env.OPEN_AI_KEY,
